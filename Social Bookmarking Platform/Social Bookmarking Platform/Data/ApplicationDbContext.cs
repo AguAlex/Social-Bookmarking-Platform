cff547913ede8249d13f6110382e056a9bad2ed3
Agusoaei Alexandru <alex.agu@yahoo.com>
2024-12-10 00:25:20 +0200

    test

---
+++ Social Bookmarking Platform/Social Bookmarking Platform/Data/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Social_Bookmarking_Platform.Models;
+
+namespace Social_Bookmarking_Platform.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>
+        options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Bookmark> Bookmarks { get; set; }
+        public DbSet<Board> Boards { get; set; }
+        public DbSet<BookmarkBoard> BookmarkBoards { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Comment> Comment { get; set; }
+        protected override void OnModelCreating(ModelBuilder
+        modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<BookmarkBoard>()
+            .HasKey(ac => new { ac.BookmarkId, ac.BoardId });
+            modelBuilder.Entity<BookmarkBoard>()
+            .HasOne(ac => ac.Bookmark)
+            .WithMany(ac => ac.BookmarkBoards)
+            .HasForeignKey(ac => ac.BookmarkId);
+            modelBuilder.Entity<BookmarkBoard>()
+            .HasOne(ac => ac.Board)
+            .WithMany(ac => ac.BookmarkBoards)
+            .HasForeignKey(ac => ac.BoardId);
+        }
+
+    }
+}
---
+++ Social Bookmarking Platform/Social Bookmarking Platform/Models/Bookmark.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Social_Bookmarking_Platform.Models
+{
+    public class Bookmark
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Titlul este obligatoriu")]
+        [StringLength(100, ErrorMessage = "Titlul nu poate avea mai mult de 100 de caractere")]
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "Categoria este obligatorie")]
+        public int? CategoryId { get; set; }
+
+        public string? UserId { get; set; }
+
+        public virtual Category? Category { get; set; }
+
+        public virtual ApplicationUser? User { get; set; }
+
+        public virtual ICollection<Comment>? Comments { get; set; }
+
+        public virtual ICollection<BookmarkBoard>? BookmarkBoards { get; set; }
+
+        [NotMapped]
+        public IEnumerable<SelectListItem>? Categ { get; set; }
+    }
+}
