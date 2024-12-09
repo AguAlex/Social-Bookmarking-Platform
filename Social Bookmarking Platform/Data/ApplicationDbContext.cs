148a32418f118b254a426f4b31ee8c9e52541add
Agusoaei Alexandru <alex.agu@yahoo.com>
2024-12-09 21:54:45 +0200

    Update ApplicationDbContext.cs

--- Social Bookmarking Platform/Data/ApplicationDbContext.cs
+++ Social Bookmarking Platform/Data/ApplicationDbContext.cs
@@ -12,5 +12,24 @@ namespace Social_Bookmarking_Platform.Data
         {
         }
 
+        public DbSet<Bookmark> Bookmarks { get; set; }
+        public DbSet<Board> Boards { get; set; }
+        public DbSet<BookmarkBoard> BookmarkBoards { get; set; }
+        protected override void OnModelCreating(ModelBuilder
+        modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<BookmarkBoard>()
+            .HasKey(ac => new { ac.BookmarkId, ac.BoardId });
+            modelBuilder.Entity<BookmarkBoard>()
+            .HasOne(ac => ac.Bookmark)
+            .WithMany(ac => ac.BookmarkBoards)
+            .HasForeignKey(ac => ac.BookmarkId);
+            modelBuilder.Entity<BookmarkBoard>()
+            .HasOne(ac => ac.Board)
+            .WithMany(ac => ac.BookmarkBoards)
+            .HasForeignKey(ac => ac.BoardId);
+        }
+
     }
 }
