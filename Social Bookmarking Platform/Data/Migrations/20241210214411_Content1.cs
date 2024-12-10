b2053406dc79ff90e60d86082230a97a9707a89c
CosminJ2004 <cosjin35@gmail.com>
2024-12-11 01:42:22 +0200

    first

---
+++ Social Bookmarking Platform/Data/Migrations/20241210214411_Content1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Social_Bookmarking_Platform.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Content1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Content",
+                table: "Bookmarks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "Bookmarks");
+        }
+    }
+}
