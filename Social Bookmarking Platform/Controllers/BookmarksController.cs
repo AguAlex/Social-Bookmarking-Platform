ff7d290f9f9f8c28024873ad55a5753f848d8ea7
Agusoaei Alexandru <alex.agu@yahoo.com>
2024-12-11 00:11:59 +0200

    motor de cautare si afisare paginata

--- Social Bookmarking Platform/Controllers/BookmarksController.cs
+++ Social Bookmarking Platform/Controllers/BookmarksController.cs
@@ -37,10 +37,89 @@ namespace Social_Bookmarking_Platform.Controllers
 
             ViewBag.Bookmarks = bookmarks;
 
+
+
             if (TempData.ContainsKey("message"))
             {
                 ViewBag.Message = TempData["message"];
                 ViewBag.Alert = TempData["messageType"];
+            }
+
+            // MOTOR DE CAUTARE
+            var search = "";
+            if (Convert.ToString(HttpContext.Request.Query["search"]) != null)
+            {
+                search = Convert.ToString(HttpContext.Request.Query["search"]).Trim(); // eliminam spatiile libere 
+                // Cautare in articol (Title)
+                List<int> bookmarkIds = db.Bookmarks.Where
+                                        (
+                                         bk => bk.Title.Contains(search)
+                                         //|| bk.Content.Contains(search)
+                                        ).Select(a => a.Id).ToList();
+                
+                // Cautare in comentarii (Content)
+                List<int> bookmarkIdsOfCommentsWithSearchString = db.Comments
+                                        .Where
+                                        (
+                                         c => c.Content.Contains(search)
+                                        ).Select(c => (int)c.BookmarkId).ToList();
+                
+
+                // Se formeaza o singura lista formata din toate id-urile selectate anterior
+                List<int> mergedIds = bookmarkIds.Union(bookmarkIdsOfCommentsWithSearchString).ToList();
+
+
+                // Lista bookmark-urilor care contin cuvantul cautat
+                // fie in articol -> Title si Content
+                // fie in comentarii -> Content
+                bookmarks = db.Bookmarks.Where(bookmark => mergedIds.Contains(bookmark.Id))
+                                      .Include("Category")
+                                      .Include("User")
+                                      .OrderByDescending(a => a.Date);
+
+            }
+
+            ViewBag.SearchString = search;
+
+            // AFISARE PAGINATA
+
+            // Alegem sa afisam 3 articole pe pagina
+            int _perPage = 6;
+
+            // Fiind un numar variabil de bookmarks, verificam de fiecare data utilizand 
+            // metoda Count()
+
+            int totalItems = bookmarks.Count();
+
+            var currentPage = Convert.ToInt32(HttpContext.Request.Query["page"]);
+
+            // Pentru prima pagina offsetul o sa fie zero
+            // Pentru pagina 2 o sa fie 3 
+            // Asadar offsetul este egal cu numarul de bookmarks care au fost deja afisate pe paginile anterioare
+            var offset = 0;
+
+            // Se calculeaza offsetul in functie de numarul paginii la care suntem
+            if (!currentPage.Equals(0))
+            {
+                offset = (currentPage - 1) * _perPage;
+            }
+
+            var paginatedBookmarks = bookmarks.Skip(offset).Take(_perPage);
+
+            // Preluam numarul ultimei pagini
+            ViewBag.lastPage = Math.Ceiling((float)totalItems / (float)_perPage);
+
+            // Trimitem bookmarks cu ajutorul unui ViewBag catre View-ul corespunzator
+            ViewBag.Bookmarks = paginatedBookmarks;
+
+
+            if (search != "")
+            {
+                ViewBag.PaginationBaseUrl = "/Bookmarks/Index/?search=" + search + "&page";
+            }
+            else
+            {
+                ViewBag.PaginationBaseUrl = "/Bookmarks/Index/?page";
             }
 
             return View();
